1d8b21dd9253024b268bef75431ee5c314922a02
Sharjeel <sharjeel.asif66@gmail.com>
2025-07-26 15:40:42 +0500

    update mock data

--- src/todo/todos/todos.ts
+++ src/todo/todos/todos.ts
@@ -6,6 +6,30 @@ const todos: TodoEntity[] = [
     completed: false,
     deadline: new Date('2025-07-26'),
   },
+  {
+    id: 2,
+    task: 'Learn NextJS',
+    completed: false,
+    deadline: new Date('2025-07-28'),
+  },
+  {
+    id: 3,
+    task: 'Learn NestJS',
+    completed: false,
+    deadline: new Date('2025-07-28'),
+  },
+  {
+    id: 4,
+    task: 'Practise DSA',
+    completed: false,
+    deadline: new Date('2025-07-28'),
+  },
+  {
+    id: 5,
+    task: 'Exercise',
+    completed: false,
+    deadline: new Date('2025-07-29'),
+  },
 ];
 
 export default todos;
