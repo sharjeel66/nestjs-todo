ca8bcd6f551c22ebcd781082bfac50880d9de8b1
Sharjeel <sharjeel.asif66@gmail.com>
2025-07-27 13:20:04 +0500

    apply dto validation globally

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,7 +1,18 @@
-import { IsBoolean, IsDateString, IsOptional, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTodoDto {
-  @IsString()
+  @IsString({ message: 'Task must be string.' })
+  @IsNotEmpty({ message: 'Task cannot be empty.' })
+  @MinLength(2, { message: 'Task must contain a minimum of 2 characters.' })
+  @MaxLength(50, { message: 'Task cannot contain more than 50 characters.' })
   task: string;
 
   @IsOptional()
