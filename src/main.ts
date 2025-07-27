8367829419f54243e04e604f911e369e7d6e5ddf
Sharjeel <sharjeel.asif66@gmail.com>
2025-07-27 15:27:55 +0500

    update dto and validation pipeline globally

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: false },
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -9,17 +9,23 @@ import {
 } from 'class-validator';
 
 export class CreateTodoDto {
-  @IsString({ message: 'Task must be string.' })
   @IsNotEmpty({ message: 'Task cannot be empty.' })
   @MinLength(2, { message: 'Task must contain a minimum of 2 characters.' })
   @MaxLength(50, { message: 'Task cannot contain more than 50 characters.' })
+  @IsString({ message: 'Task must be a string.' })
   task: string;
 
   @IsOptional()
-  @IsBoolean()
+  @IsBoolean({ message: 'Completed must be a boolean value.' })
   completed?: boolean;
 
   @IsOptional()
-  @IsDateString()
+  @IsDateString(
+    {},
+    {
+      message:
+        'Deadline must be a valid ISO 8601 date string in YYYY-MM-DD (e.g., 2025-08-13 or 2025-08-03T15:30:00Z).',
+    },
+  )
   deadline?: string;
 }
